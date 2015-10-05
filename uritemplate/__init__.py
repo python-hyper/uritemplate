995f88f79a4881df48af3e230c5ed9321176d071
Ian Cordasco <graffatcolmingov@gmail.com>
2015-10-04 20:45:10 -0500

    Update copyright, version, and __all__

--- uritemplate/__init__.py
+++ uritemplate/__init__.py
@@ -7,7 +7,7 @@ The URI templating library for humans.
 
 See http://uritemplate.rtfd.org/ for documentation
 
-:copyright: (c) 2013 Ian Cordasco
+:copyright: (c) 2013-2015 Ian Cordasco
 :license: Modified BSD, see LICENSE for more details
 
 """
@@ -16,9 +16,9 @@ __title__ = 'uritemplate'
 __author__ = 'Ian Cordasco'
 __license__ = 'Modified BSD'
 __copyright__ = 'Copyright 2013 Ian Cordasco'
-__version__ = '0.3.0'
+__version__ = '0.3.1'
 __version_info__ = tuple(int(i) for i in __version__.split('.'))
 
 from uritemplate.api import URITemplate, expand, partial
 
-__all__ = [URITemplate, expand, partial]
+__all__ = ('URITemplate', 'expand', 'partial')
