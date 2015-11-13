925ffa28d44f701c3831340c46151078656eef2c
Ian Cordasco <graffatcolmingov@gmail.com>
2015-11-13 08:28:32 -0600

    Fix one minor style nit
    I always prefer parens to backslashes

--- uritemplate/variable.py
+++ uritemplate/variable.py
@@ -346,8 +346,9 @@ class URIVariable(object):
 
 
 def is_list_of_tuples(value):
-    if not value or not isinstance(value, (list, tuple)) or \
-            not all(isinstance(t, tuple) and len(t) == 2 for t in value):
+    if (not value or
+            not isinstance(value, (list, tuple)) or
+            not all(isinstance(t, tuple) and len(t) == 2 for t in value)):
         return False, None
 
     return True, value
