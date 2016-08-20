862e89fb5ccfee7764f84d2aa15ffd140a0dbbd4
Ian Cordasco <graffatcolmingov@gmail.com>
2016-08-20 07:57:34 -0500

    Handle RC1 in the version string

--- uritemplate/__init__.py
+++ uritemplate/__init__.py
@@ -17,7 +17,7 @@ __author__ = 'Ian Cordasco'
 __license__ = 'Modified BSD or Apache License, Version 2.0'
 __copyright__ = 'Copyright 2013 Ian Cordasco'
 __version__ = '2.0.0rc1'
-__version_info__ = tuple(int(i) for i in __version__.split('.'))
+__version_info__ = tuple(int(i) for i in __version__.split('.') if i.isdigit())
 
 from uritemplate.api import URITemplate, expand, partial  # noqa: E402
 
