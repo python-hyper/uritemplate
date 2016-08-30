8274b1524f0a5467da13d3ee32401aea5083b0e0
Ian Cordasco <graffatcolmingov@gmail.com>
2016-08-29 19:11:46 -0500

    Release v2.0.0 of uritemplate

--- uritemplate/__init__.py
+++ uritemplate/__init__.py
@@ -16,7 +16,7 @@ __title__ = 'uritemplate'
 __author__ = 'Ian Cordasco'
 __license__ = 'Modified BSD or Apache License, Version 2.0'
 __copyright__ = 'Copyright 2013 Ian Cordasco'
-__version__ = '2.0.0rc2'
+__version__ = '2.0.0'
 __version_info__ = tuple(int(i) for i in __version__.split('.') if i.isdigit())
 
 from uritemplate.api import (
